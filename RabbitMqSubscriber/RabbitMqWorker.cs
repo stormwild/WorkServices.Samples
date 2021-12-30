65cf01a612eee500cf11edbb19ea33a8f23a37ce
Alexander R Torrijos <artorrijos@gmail.com>
2021-12-30 21:33:23 +0800

    RabbitMq sample: work-in-progress

---
+++ RabbitMqSubscriber/RabbitMqWorker.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace RabbitMqSubscriber
+{
+    public class RabbitMqWorker : BackgroundService
+    {
+        private readonly ILogger<RabbitMqWorker> _logger;
+
+        public RabbitMqWorker(ILogger<RabbitMqWorker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WorkerServiceDemo/Program.cs
+++ WorkerServiceDemo/Program.cs
@@ -1,10 +1,12 @@
-﻿using WorkerServiceDemo;
+﻿using RabbitMqSubscriber;
+using WorkerServiceDemo;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
         services.AddHostedService<TimerWorker>();
+        services.AddHostedService<RabbitMqWorker>();
     })
     .Build();
 
